c010f013b1bd0af8453becc6b3e6f4cd7a81498b
Operator Beamline Xaloc <opbl13@pcbl1307.cells.es>
2021-03-17 18:14:01 +0100

    Bug fix for not reading initial motor position in the motor widgets

--- BlissFramework/Bricks/Qt4_MotorSpinBoxBrick.py
+++ BlissFramework/Bricks/Qt4_MotorSpinBoxBrick.py
@@ -665,7 +665,8 @@ class Qt4_MotorSpinBoxBrick(BlissWidget):
                          'stateChanged',
                          self.state_changed,
                          instanceFilter=True)
-
+            
+        self.position_changed(self.motor_hwobj.getPosition())
         self.pos_history = []
         self.update_gui()
         #self['label'] = self['label']
